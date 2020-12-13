ed4e8fe7de9e8faa2a38de07c034d825a70a5dc4
Salami <Salamesalehpour@gmail.com>
2020-12-13 22:26:44 +0330

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static SamuraiContext context = new SamuraiContext();
+        static void Main(string[] args)
+        {
+            context.Database.EnsureCreated();
+            GetSamurais("Before Add...");
+
+            AddSamurai("Salam");
+            AddSamurai("Barin");
+
+            GetSamurais("After Add...");
+
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurai(string samuraiName)
+        {
+            var salamSamurai = new Samurai { Name = samuraiName };
+            context.Samurais.Add(salamSamurai);
+            context.SaveChanges();
+        }
+
+        private static void GetSamurais(string text)
+        {
+            var samurais = context.Samurais.ToList();
+
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
+        }
+    }
+}
